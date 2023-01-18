cd5a9d4c7763aa3c624954f03e1c42dcf6d6bd0e
vnatyreromchiss <rdg2039@gmail.com>
2023-01-17 18:24:00 -0600

    chill

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Minus</button>
+        <button onClick={onClickPlus} className="plus">Plus +</button>
+      </div>
     </div>
   );
 }
